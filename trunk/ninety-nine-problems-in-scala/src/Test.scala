b2f0475e81903acca24c08ba5e9c07df167403b3
Muqi Li <lmq3030@gmail.com>
2012-11-13 01:01:46 -0500

    Add first package and solutions to p1-p5

--- trunk/ninety-nine-problems-in-scala/src/Test.scala
+++
@@ -1,5 +0,0 @@
-object Test {
-   def main(args: Array[String]) {
-    println("Scala is a wonderful language !")
-  }
-}
---
+++ trunk/ninety-nine-problems-in-scala/src/list/P4.scala
@@ -0,0 +1,17 @@
+package list
+// Find the number of elements of a list.
+object P4 {
+  def main(args: Array[String]) {
+    println(len(List(1, 2, 3)))
+    println(len(List(1)))
+    println(len(List()))
+
+  }
+  
+  def len[A](list: List[A]) : Int = {
+    list match {
+      case Nil => 0
+      case x::xs => 1 + len(xs)
+    }
+  }
+}
